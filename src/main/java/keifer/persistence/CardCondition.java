bb110e7859e9f77487a89d0059912ac550982f81
unknown <lewiskeifer@yahoo.com>
2019-04-27 14:24:39 -0400

    add sql layer

---
+++ src/main/java/keifer/persistence/CardCondition.java
@@ -0,0 +1,11 @@
+package keifer.persistence;
+
+public enum CardCondition {
+
+    NEAR_MINT,
+    LIGHT_PLAY,
+    MODERATE_PLAY,
+    HEAVY_PLAY,
+    DAMAGED;
+
+}
---
+++ src/main/java/keifer/persistence/CardRepository.java
@@ -0,0 +1,7 @@
+package keifer.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CardRepository extends CrudRepository<CardEntity, Integer> {
+
+}
