244dd6209d2df8f98f4cacae03e485dbd49201d2
Keifer <lewiskeifer@yahoo.com>
2019-06-28 13:15:09 -0400

    modify sessioncontext service

--- src/main/java/keifer/service/SessionContextService.java
+++ src/main/java/keifer/service/SessionContextService.java
@@ -1,18 +1,25 @@
 package keifer.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Service
 public class SessionContextService {
 
-    // example usage
-    public HttpSession getSession() {
-        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
-        return attr.getRequest().getSession(true); // true == allow create
+    private final HttpServletRequest httpServletRequest;
+
+    @Autowired
+    public SessionContextService(HttpServletRequest httpServletRequest) {
+        this.httpServletRequest = httpServletRequest;
+    }
+
+    public String getSession() {
+        return httpServletRequest.getHeader("authorization");
     }
 
 }
